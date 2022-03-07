71a4034372da93a87fea4f79de53027b1649327b
alvarodev-lc <alvaro.lopez19997@gmail.com>
2022-03-07 19:12:11 +0100

    Initial Commit

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,20 @@
+import React, {Componenet} from 'react'
+
+class Navbar extends Component {
+    render(){
+        return(
+            <nav className="navbar-items">
+                <h1 className="navbar-logo">
+                    React
+                </h1>
+                <div className="menu-icon">
+                        
+                </div>
+                <ul>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
