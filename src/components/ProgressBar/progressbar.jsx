3dbbcccbd839bb7b59d754c9ea5d6135cafb3c37
alvarodev-lc <alvaro.lopez19997@gmail.com>
2022-03-26 15:39:16 +0100

    Prettify ProgressBar component code

--- src/components/ProgressBar/progressbar.jsx
+++ src/components/ProgressBar/progressbar.jsx
@@ -4,14 +4,14 @@ function ProgressBar(props) {
     // To set the progressbar text dynamically
     let percentage = props.progress;
     // Create necessary css to create the fill of the progressbar
+    let fill_classname = `progressbar-fill-${percentage}`
     const css = `
-    .progressbar-fill-${percentage} {
+    .${fill_classname} {
         width: ${percentage}%;
         height: 100%;
         background-color: pink;
     }
     `
-    let fill_classname = `progressbar-fill-${percentage}`
     console.log(css)
     return(
         <div className="progressbar">
