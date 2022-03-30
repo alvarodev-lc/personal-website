2ea21992155a61490a830572616ba6b149bb05af
alvarodev-lc <alvaro.lopez19997@gmail.com>
2022-03-30 22:59:56 +0200

    Fix index code indentation

--- src/index.js
+++ src/index.js
@@ -9,28 +9,13 @@ import {
     Route,
 } from "react-router-dom";
 
-ReactDOM.render( <
-    BrowserRouter >
-    <
-    Routes Routes >
-
-    <
-    Route exact path = '/'
-    element = { < Loading / > }
-    /> 
-
-    <
-    Route path = '/about'
-    element = { < About / > }
-    />
-
-    <
-    Route path = '/home'
-    element = { < App / > }
-    />
-
-    <
-    /Routes >  < /
-    BrowserRouter > ,
+ReactDOM.render( 
+    <BrowserRouter>
+        <Routes Routes>
+            <Route exact path = '/' element = { <Loading/> }/> 
+            <Route path = '/about' element = { <About/> }/>
+            <Route path = '/home' element = {<App/> }/>
+        </Routes >  
+    </BrowserRouter > ,
     document.getElementById('App')
 );
