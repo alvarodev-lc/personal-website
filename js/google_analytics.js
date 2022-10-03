4943e4fd6446fc4ee25e0f92254440da7047aa02
alvarodev-lc <89037751+alvarodev-lc@users.noreply.github.com>
2022-10-04 01:30:55 +0200

    Delete js directory

--- js/google_analytics.js
+++
@@ -1,9 +0,0 @@
-var imported = document.createElement('script');
-imported.src = 'https://www.googletagmanager.com/gtag/js?id=G-9WHKXZZVXY';
-document.head.appendChild(imported);
-
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-
-gtag('config', 'G-9WHKXZZVXY');
